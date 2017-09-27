04e8e783af20e378475f9ecf2e70fcebbdcfc53c
Dion Durigon <dion.durigon@rubylife.com>
2017-09-27 14:10:52 -0400

    Formatting updates

--- Clean Swift/View Controller.xctemplate/UICollectionViewController/___FILEBASENAME___ViewController.swift
+++ Clean Swift/View Controller.xctemplate/UICollectionViewController/___FILEBASENAME___ViewController.swift
@@ -12,78 +12,69 @@
 
 import UIKit
 
-protocol ___VARIABLE_sceneName___DisplayLogic: class
-{
-  func displaySomething(viewModel: ___VARIABLE_sceneName___.Fetch.ViewModel)
+protocol ___VARIABLE_sceneName___DisplayLogic: class {
+    func displaySomething(viewModel: ___VARIABLE_sceneName___.Fetch.ViewModel)
 }
 
-class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VARIABLE_sceneName___DisplayLogic
-{
-  var interactor: ___VARIABLE_sceneName___BusinessLogic?
-  var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
+class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VARIABLE_sceneName___DisplayLogic {
+    var interactor: ___VARIABLE_sceneName___BusinessLogic?
+    var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
 
-  // MARK: Object lifecycle
+    // MARK: Object lifecycle
 
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-
-  required init?(coder aDecoder: NSCoder)
-  {
-    super.init(coder: aDecoder)
-    setup()
-  }
-
-  // MARK: Setup
-
-  private func setup()
-  {
-    let viewController = self
-    let interactor = ___VARIABLE_sceneName___Interactor()
-    let presenter = ___VARIABLE_sceneName___Presenter()
-    let router = ___VARIABLE_sceneName___Router()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-
-  // MARK: Routing
-
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
     }
-  }
 
-  // MARK: View lifecycle
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
 
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    doSomething()
-  }
+    // MARK: Setup
 
-  // MARK: Do something
+    private func setup() {
+        let viewController = self
+        let interactor = ___VARIABLE_sceneName___Interactor()
+        let presenter = ___VARIABLE_sceneName___Presenter()
+        let router = ___VARIABLE_sceneName___Router()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
 
-  //@IBOutlet weak var nameTextField: UITextField!
+    // MARK: Routing
 
-  func doSomething()
-  {
-    let request = ___VARIABLE_sceneName___.Fetch.Request()
-    interactor?.doSomething(request: request)
-  }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
 
-  func displaySomething(viewModel: ___VARIABLE_sceneName___.Fetch.ViewModel)
-  {
-    //nameTextField.text = viewModel.name
-  }
+    // MARK: View lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        doSomething()
+    }
+
+    // MARK: Do something
+
+    //@IBOutlet weak var nameTextField: UITextField!
+
+    func doSomething() {
+        let request = ___VARIABLE_sceneName___.Fetch.Request()
+        interactor?.doSomething(request: request)
+    }
+
+    func displaySomething(viewModel: ___VARIABLE_sceneName___.Fetch.ViewModel) {
+        //nameTextField.text = viewModel.name
+    }
 }
